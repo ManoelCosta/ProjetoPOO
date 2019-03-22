44b5840ad46e1805dc30a01d600fa30c3b33a21c
Manoel Costa <ManoelCosta>
2019-03-22 18:41:05 -0300

    Função cadPaciente funcionando

--- src/ProjetoClinica/AtendimentoMedico.java
+++ src/ProjetoClinica/AtendimentoMedico.java
@@ -8,5 +8,41 @@ public class AtendimentoMedico {
     Medico medico;
     Paciente paciente;
 
+    void cadPaciente(Paciente paciente){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe o nome");
+        paciente.nome = sc.next();
+        System.out.println("Informe o CPF: ");
+        paciente.cpf = sc.nextInt();
+        System.out.println("Informe o RG: ");
+        paciente.rg = sc.nextInt();
+        System.out.println("Informe a idade: ");
+        paciente.idade = sc.nextInt();
+        System.out.println("Informe o gênero: ");
+        paciente.genero = sc.next();
+        System.out.println("Informe o Estado Civil: ");
+        paciente.estadoCivil = sc.next();
+        System.out.println("Informe a profissão do paciente: ");
+        paciente.profissao = sc.next();
+        System.out.println("Informe o Peso: ");
+        paciente.peso = sc.nextDouble();
+        System.out.println("Informe a altura: ");
+        paciente.altura = sc.nextDouble();
+        System.out.println("Informe o CEP: ");
+        paciente.cep = sc.nextInt();
+        System.out.println("Informe a rua: ");
+        paciente.rua = sc.next();
+        System.out.println("Informe o Bairro: ");
+        paciente.bairro = sc.next();
+        System.out.println("Informe a cidade: ");
+        paciente.cidade = sc.next();
+        System.out.println("Informe o Estado: ");
+        paciente.estado = sc.next();
+        System.out.println("Informe a Pressão arterial");
+        System.out.print("Sistole: ");
+        paciente.pressaoSistolica = sc.nextInt();
+        System.out.print("Diastole: ");
+        paciente.pressaoDiastolica = sc.nextInt();
 
+    }
 }
--- src/ProjetoClinica/Clinica.java
+++ src/ProjetoClinica/Clinica.java
@@ -11,39 +11,8 @@ public class Clinica {
 
         //Cadastro de paciente
         Scanner sc = new Scanner(System.in);
-        System.out.print("Informe o nome do Paciente: ");
-        paciente.nome = sc.next();
-        System.out.println("Informe a idade do Paciente: ");
-        paciente.idade = sc.nextInt();
-        System.out.println("Informe o CPF do paciente: ");
-        paciente.cpf = sc.nextInt();
-        System.out.println("Informe o RG do paciente: ");
-        paciente.rg = sc.nextInt();
-        System.out.println("Informe o gênero do Paciente: ");
-        paciente.genero = sc.next();
-        System.out.println("Informe o estado civil do paciente: ");
-        paciente.estadoCivil = sc.next();
-        System.out.println("Informe a profissão do paciente: ");
-        paciente.profissao = sc.next();
-        System.out.println("Informe o CEP do Paciente: ");
-        paciente.cep = sc.nextInt();
-        System.out.println("Informe a rua do paciente: ");
-        paciente.rua = sc.next();
-        System.out.println("Informe o bairro: ");
-        paciente.bairro = sc.next();
-        System.out.print("Informe a cidade do paciente: ");
-        paciente.cidade = sc.next();
-        System.out.print("Informe o Estado do paciente");
-        paciente.estado = sc.next();
-        System.out.print("Informe a altura do paciente: ");
-        paciente.altura = sc.nextDouble();
-        System.out.print("Informe o peso do paciente: ");
-        paciente.peso = sc.nextDouble();
-        System.out.println("Informe a pressão arterial do paciente");
-        System.out.print("Sistole: ");
-        paciente.pressaoSistolica = sc.nextInt();
-        System.out.print("Diastole: ");
-        paciente.pressaoDiastolica = sc.nextInt();
+
+        atendimento.cadPaciente(paciente);
 
 
     }
