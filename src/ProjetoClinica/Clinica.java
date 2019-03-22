6e9d9e94d58c54467e0f7d01e2a09f5caf7d0587
ManoelCosta <ManoelCosta>
2019-03-22 11:43:47 -0300

    Cadastro de paciente

--- src/ProjetoClinica/Clinica.java
+++ src/ProjetoClinica/Clinica.java
@@ -3,6 +3,7 @@ package ProjetoClinica;
 import java.util.Scanner;
 
 public class Clinica {
+
     public static void main (String args[]){
         Paciente paciente = new Paciente();
         Medico medico = new Medico();
